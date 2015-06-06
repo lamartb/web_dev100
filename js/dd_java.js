84d0080e09e9dbef29c99ff47ed5b6366bd5486b
Lamar Bryant <lamarsmurf@gmail.com>
2015-06-06 14:05:21 -0400

    first inital comment or first version upload

---
+++ js/dd_java.js
@@ -0,0 +1,21 @@
+//
+var dude = 30;
+var homey = 4;
+var friTotal = dude + homey;
+
+console.log( "friTotal:" + dude);
+
+
+//2
+
+var fnum = "Lamar";
+var lnum = "Bryant";
+
+alert(fnum + lnum);
+
+
+//3
+
+var frontEnd = ["HTML", "CSS", "JS"];
+var frontEndCon = ["Content and Structure", "Aesthetics", "Information"];
+var frontEnd = []
